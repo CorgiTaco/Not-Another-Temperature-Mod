2ba54310f3cd70609423ffac4d89e8e1999cf7fa
CorgiTaco <>
2020-09-12 00:17:39 -0700

    Setup client side methods

--- src/main/java/corgitaco/notanothertempmod/NotAnotherTemperatureMod.java
+++ src/main/java/corgitaco/notanothertempmod/NotAnotherTemperatureMod.java
@@ -1,5 +1,10 @@
 package corgitaco.notanothertempmod;
 
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityViewRenderEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,5 +13,35 @@ import org.apache.logging.log4j.Logger;
 public class NotAnotherTemperatureMod {
     public static final Logger LOGGER = LogManager.getLogger();
 
+    public static final String MOD_ID = "notanothertempmod";
+
     public NotAnotherTemperatureMod() {}
+
+
+
+
+
+
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class NATMClientEvents {
+
+        @SubscribeEvent
+        public static void renderTickEvent(TickEvent.RenderTickEvent event) {}
+
+        @SubscribeEvent
+        public static void clientTickEvent(TickEvent.ClientTickEvent event) {}
+
+        @SubscribeEvent
+        public static void renderFogEvent(EntityViewRenderEvent.FogDensity event) {}
+
+        @SubscribeEvent
+        public void renderGameOverlayEventPre(RenderGameOverlayEvent.Pre event) {}
+
+        @SubscribeEvent
+        public void renderGameOverlayEventPost(RenderGameOverlayEvent.Post event) {}
+
+        @SubscribeEvent
+        public void renderGameOverlayEventText(RenderGameOverlayEvent.Text event) {}
+    }
 }
