cb0e250717ab77102fd189577c397a54a71724c6
CorgiTaco <>
2020-09-12 00:04:32 -0700

    Init forge branch

---
+++ src/main/java/corgitaco/notanothertempmod/NotAnotherTemperatureMod.java
@@ -0,0 +1,12 @@
+package corgitaco.notanothertempmod;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("notanothertempmod")
+public class NotAnotherTemperatureMod {
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public NotAnotherTemperatureMod() {}
+}
---
+++ src/main/java/corgitaco/notanothertempmod/mixin/MixinConnector.java
@@ -0,0 +1,12 @@
+package corgitaco.notanothertempmod.mixin;
+
+import org.spongepowered.asm.mixin.Mixins;
+import org.spongepowered.asm.mixin.connect.IMixinConnector;
+
+public class MixinConnector implements IMixinConnector {
+
+    @Override
+    public void connect() {
+        Mixins.addConfiguration("notanothertempmod.mixins.json");
+    }
+}
