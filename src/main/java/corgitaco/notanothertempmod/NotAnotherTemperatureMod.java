3ffcf9ea37df4a8aa0e02306a1569c994160a559
CorgiTaco <>
2020-09-12 00:21:20 -0700

    Setup remaining events

--- src/main/java/corgitaco/notanothertempmod/NotAnotherTemperatureMod.java
+++ src/main/java/corgitaco/notanothertempmod/NotAnotherTemperatureMod.java
@@ -4,8 +4,12 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -15,12 +19,28 @@ public class NotAnotherTemperatureMod {
 
     public static final String MOD_ID = "notanothertempmod";
 
-    public NotAnotherTemperatureMod() {}
+    public NotAnotherTemperatureMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
 
+    }
 
+    public void commonSetup(FMLCommonSetupEvent event) {}
 
+    public void clientSetup(FMLClientSetupEvent event) {}
 
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class NATMEvents {
 
+        @SubscribeEvent
+        public static void playerTickEvent(TickEvent.PlayerTickEvent event) {}
+
+        @SubscribeEvent
+        public static void worldTickEvent(TickEvent.WorldTickEvent event) {}
+
+        @SubscribeEvent
+        public static void entityTickEvent(LivingEvent.LivingUpdateEvent event) {}
+    }
 
 
     @Mod.EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
